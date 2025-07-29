09ada9702e6c2c26e21ab010baa67c8f2c83246c
Hasan <hassaan098760@gmail.com>
2025-07-29 17:34:11 +0600

    Incident step 5  complete

--- src/Components/IncidentsStep5.jsx
+++ src/Components/IncidentsStep5.jsx
@@ -10,6 +10,25 @@ function IncidentsStep5() {
     <Herolayout />
    <Incidehero nextPath="/IncidentsStep2" />
 
+   
+<div className='flex flex-col items-center justify-center mt-20 mb-30 '>
+
+
+   <div className=' flex flex-col justify-start p-10'>
+
+ <div className=' flex flex-col justify-start items-start gap-2 mb-8'>
+<h1 className='text-4xl font-semibold text-navactive mb-5'>Where’s the incident?</h1>
+<p className='text-nav '>Enter a GPS, address. or pin point on the map. Try to be as accurate as<br/> possible, or click:Jurisdiction Wide</p>
+    </div>
+    <div className=''>
+<img src="/map.png" alt="image" />
+    </div>
+
+   </div>
+
+
+</div>
+
     </>
   )
 }
