82a32e252a06b53852fe96030d37f451a2edf58b
Hasan <hassaan098760@gmail.com>
2025-07-29 16:55:24 +0600

    full Incidents Home section

--- src/Components/Incidents.jsx
+++ src/Components/Incidents.jsx
@@ -1,8 +1,54 @@
 import React from 'react'
 import Hero from './Hero';
+import Card from './Card';
 
 
 
+
+const Data = [
+  {
+    image: '/card1.png',
+    title: 'Whitechapel Rd.',
+    location: 'Tulare County, Los Angeles, CA 23415',
+    price: 1456654,
+    tag: 'Blizzard',
+  },
+  {
+    image: '/card2.png',
+    title: 'Whitechapel Rd.',
+    location: 'Tulare County, Los Angeles, CA 23415',
+    price: 1456654,
+    tag: 'Blizzard',
+  },
+  {
+    image: '/card3.png',
+    title: 'Tulare County',
+    location: 'Tulare County, Los Angeles, CA 23415',
+    price: 1456654,
+    tag: 'Blizzard',
+  },
+  {
+    image: '/card4.png',
+    title: 'Tulare County',
+    location: 'Tulare County, Los Angeles, CA 23415',
+    price: 1456654,
+    tag: 'Blizzard',
+  },
+  {
+    image: '/card5.png',
+    title: 'Tulare County',
+    location: 'Tulare County, Los Angeles, CA 23415',
+    price: 1456654,
+    tag: 'Blizzard',
+  },
+  {
+    image: '/card6.png',
+    title: 'Tulare County',
+    location: 'Tulare County, Los Angeles, CA 23415',
+    price: 1456654,
+    tag: 'Blizzard',
+  },
+];
 
 
 function Incidents() {
@@ -11,6 +57,16 @@ function Incidents() {
   return (
    <>
    <Hero title="Home-Incidents" subtitle="Incidents" buttontext="+ New Incidents" buttonLink="/Incidents/IncidentsStep2"/>
+
+
+
+       <div className="p-20 min-h-screen mb-56">
+      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-6">
+        {Data.map((item, index) => (
+          <Card key={index} {...item} />
+        ))}
+      </div>
+    </div>
 
    </>
   )
